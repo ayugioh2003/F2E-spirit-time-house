a3365e33ad5d8f04aa964fcc8760269696aca1f2
chu <ayugioh2003@gmail.com>
2018-06-10 16:27:27 +0800

    add basic UI

--- No1.todolist/js/all.js
+++ No1.todolist/js/all.js
@@ -1,6 +1,24 @@
 $(document).ready(function () {
   
 
+  // add task
+  $('.addtodo-simple input').focus(function (e) { 
+    e.preventDefault();
+    $('.addtodo-complicate').show();
+  });
+
+  $('.addtodo-simple input').blur(function (e) { 
+    e.preventDefault();
+    $('.addtodo-complicate').hide();
+  });
+
+
+  // complete task
+  $('.checkbox').click(function (e) { 
+    e.preventDefault();
+    $(this).toggleClass('complete');
+    $(this).siblings('.todoname').toggleClass('complete');
+  });
 
 
 
